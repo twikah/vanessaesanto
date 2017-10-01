69357131d5aaae34559c5f3a87becf968e9b2d1d
Vanessa Espírito Santo <vanessaesanto@gmail.com>
2017-10-01 18:54:08 +1100

    Add controllers and set routes

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def projects
+  end
+
+  def contact
+  end
+
+  def blog
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main#index'
+
+  get '/about', to: 'main#about'
+  get '/contact', to: 'main#contact'
+  get '/blog', to: 'main#blog'
+
+  resources :projects, only: [:index, :show]
 end
