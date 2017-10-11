a58b8f6185ad493f2dda9a289a79526d1a23a3ec
Vanessa Espírito Santo <vanessaesanto@gmail.com>
2017-10-11 21:54:46 +1100

    Add gravatar and description to about page

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,8 @@
 module MainHelper
+  # Returns the Gravatar for the given user.
+  def gravatar_for(email, size:80)
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, class: "gravatar")
+  end
 end
