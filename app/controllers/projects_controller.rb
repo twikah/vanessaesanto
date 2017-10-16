02c6017d575779af6978039c3069fd6513fde25d
Vanessa Espírito Santo <vanessaesanto@gmail.com>
2017-10-16 22:22:09 +1100

    Start projects index and show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,15 @@
 class ProjectsController < ApplicationController
+  @@projects = [{id: "1-website", name: "This Website"}]
+
   def index
+    @projects = @@projects
   end
 
   def show
+    @project = @@projects.find { |project| project[:id] == params[:id] }
+
+    if @project.empty?
+      return render text: "Ooops! We couldn't find that project!", status: 404
+    end
   end
 end
