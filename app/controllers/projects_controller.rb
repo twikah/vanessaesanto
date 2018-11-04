cdbb09f095ff8e4dd055b5cfa495c2c51413f341
twikah <vanessaesanto@gmail.com>
2018-11-04 21:34:42 +0100

    Update gemfile and add project 2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
-  PROJECTS = [{id: "1-website", name: "This Website", description: "Find out what led me to start my website and how did I do it."}]
+  PROJECTS = [{id: "1-website", name: "This Website", description: "Find out what led me to start my website and how did I do it."},
+               id: "2-mls", name: "MLS Website", description: "See the very first website I made professionally, what challenges it brought me and how I overcame them."]
 
   def index
     @projects = PROJECTS
