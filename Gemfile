711bd13b8c02c3c6aa187e9d4e8d72cfeb7ef025
Vanessa Santo <vanessaesanto@gmail.com>
2020-03-25 22:58:11 +0000

    Add Outflix project

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ end
 ruby '2.6.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.0'
+gem 'rails', '~> 5.2.4.2'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,35 @@
 class ProjectsController < ApplicationController
-  PROJECTS = [{id: "1-website", name: "This Website", description: "Find out what led me to start my website and how did I do it."},
-              {id: "2-mls", name: "MLS Website", description: "See the very first website I made professionally, what challenges it brought me and how I overcame them."},
-              {id: "3-booking", name: "Booking Platform", description: "I developed a booking and invoicing platform as part of MLS website's back-office."}]
+  PROJECTS = [{
+                id: '1-website',
+                name: 'This Website',
+                description: 'Find out what led me to start my website and how
+                              did I do it.'
+              },
+              {
+                id: '2-mls',
+                name: 'MLS Website',
+                description: 'See the very first website I made professionally,
+                              what challenges it brought me and how I overcame
+                              them.'
+              },
+              {
+                id: '3-booking',
+                name: 'Booking Platform',
+                description: 'I developed a booking and invoicing platform as
+                              part of MLS website\'s back-office.'
+              },
+              # {
+                # id: '4-secret-sessions',
+                # name: 'Secret Sessions',
+                # description: 'Secret Sessions is a marketplace for promoting
+                #               movie screenings in secret and special locations.'
+              # },
+              {
+                id: '5-outflix',
+                name: 'Outflix',
+                description: 'Outflix integrates with an unofficial Netflix API
+                              to track and show expiring content.'
+              }]
 
   def index
     @projects = PROJECTS
